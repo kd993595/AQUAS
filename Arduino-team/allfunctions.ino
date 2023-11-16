8c751eaccb6db1f5f3ec87ce4615aeb5b9ed77d9
kd993595 <kepedu17@gmail.com>
2023-11-16 08:25:38 -0500

    update all sensors code

--- Arduino-team/allfunctions.ino
+++ Arduino-team/allfunctions.ino
@@ -31,6 +31,8 @@ Stepper myStepper = Stepper(stepsPerRevolution, 8, 9, 10, 11);
 int state = 1;
 
 bool clockwise = true;
+unsigned long previousMillis = 0; 
+unsigned long currentMillis = 0; 
 
 void setup()
 {
@@ -50,92 +52,95 @@ void setup()
 
 void loop()
 {
+  currentMillis = millis();
 
-  if(state == 0){
-    Serial.println("case 0");
-  }else if(state == 1){
-    delay(100);
-    Serial.println("case 1");
-    // pH Measurement
-    int buf[10];
-    for (int i = 0; i < 10; i++)
-    {
-      buf[i] = analogRead(pH_SensorPin);
-      delay(10);
-    }
-    for (int i = 0; i < 9; i++)
-    {
-      for (int j = i + 1; j < 10; j++)
+  if (currentMillis - previousMillis >= 2000){//replaces delay function so loop always running
+    previousMillis = currentMillis;
+    if(state == 0){
+      Serial.println("case 0");
+    }else if(state == 1){
+      delay(100);
+      Serial.println("case 1");
+      // pH Measurement
+      int buf[10];
+      for (int i = 0; i < 10; i++)
       {
-        if (buf[i] > buf[j])
+        buf[i] = analogRead(pH_SensorPin);
+        delay(10);
+      }
+      for (int i = 0; i < 9; i++)
+      {
+        for (int j = i + 1; j < 10; j++)
         {
-          int temp = buf[i];
-          buf[i] = buf[j];
-          buf[j] = temp;
+          if (buf[i] > buf[j])
+          {
+            int temp = buf[i];
+            buf[i] = buf[j];
+            buf[j] = temp;
+          }
         }
       }
-    }
-    avgValue = 0;
-    for (int i = 2; i < 8; i++)
-      avgValue += buf[i];
+      avgValue = 0;
+      for (int i = 2; i < 8; i++)
+        avgValue += buf[i];
 
-    float phValue = (float)avgValue * 5.0 / 1024 / 6;
-    phValue = 3.5 * phValue + Offset;
+      float phValue = (float)avgValue * 5.0 / 1024 / 6;
+      phValue = 3.5 * phValue + Offset;
 
-    // Temperature Measurement
-    sensors.requestTemperatures();
-    float tempC = sensors.getTempCByIndex(0);
-    float tempF = (tempC * 9.0) / 5.0 + 32.0;
+      // Temperature Measurement
+      sensors.requestTemperatures();
+      float tempC = sensors.getTempCByIndex(0);
+      float tempF = (tempC * 9.0) / 5.0 + 32.0;
 
-    // TDS Measurement
-    static unsigned long analogSampleTimepoint = millis();
-    if (millis() - analogSampleTimepoint > 40U)
-    {
-      analogSampleTimepoint = millis();
-      analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);
-      analogBufferIndex++;
-      if (analogBufferIndex == SCOUNT)
-        analogBufferIndex = 0;
-    }
-    static unsigned long printTimepoint = millis();
-    if (millis() - printTimepoint > 800U)
-    {
-      printTimepoint = millis();
-      for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++)
-        analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
-      averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * VREF / 1024.0;
-      float compensationCoefficient = 1.0 + 0.02 * (tempC - 25.0);
-      float compensationVoltage = averageVoltage / compensationCoefficient;
-      tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
+      // TDS Measurement
+      static unsigned long analogSampleTimepoint = millis();
+      if (millis() - analogSampleTimepoint > 40U)
+      {
+        analogSampleTimepoint = millis();
+        analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);
+        analogBufferIndex++;
+        if (analogBufferIndex == SCOUNT)
+          analogBufferIndex = 0;
+      }
+      static unsigned long printTimepoint = millis();
+      if (millis() - printTimepoint > 800U)
+      {
+        printTimepoint = millis();
+        for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++)
+          analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+        averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * VREF / 1024.0;
+        float compensationCoefficient = 1.0 + 0.02 * (tempC - 25.0);
+        float compensationVoltage = averageVoltage / compensationCoefficient;
+        tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
 
-      // Print results to Serial
-      Serial.print("Temperature: ");
-      Serial.print(tempC);
-      Serial.print("C | ");
-      Serial.print(tempF);
-      Serial.print("F | pH: ");
-      Serial.print(phValue, 2);
-      Serial.print(" | TDS Value: ");
-      Serial.print(tdsValue, 0);
-      Serial.println("ppm");
-    }
-    //state = 2;
-  }else if(state==2){
-    Serial.println("case 2");
-    if(clockwise){
-      myStepper.step(200);
-      clockwise = false;
+        // Print results to Serial
+        Serial.print("Temperature: ");
+        Serial.print(tempC);
+        Serial.print("C | ");
+        Serial.print(tempF);
+        Serial.print("F | pH: ");
+        Serial.print(phValue, 2);
+        Serial.print(" | TDS Value: ");
+        Serial.print(tdsValue, 0);
+        Serial.println("ppm");
+      }
+      //state = 2;
+    }else if(state==2){
+      Serial.println("case 2");
+      if(clockwise){
+        myStepper.step(200);
+        clockwise = false;
+      }else{
+        myStepper.step(-200);
+        clockwise = true;
+      }
+      state=1;
     }else{
-      myStepper.step(-200);
-      clockwise = true;
+      Serial.println("default case");
     }
-    state=1;
-  }else{
-    Serial.println("default case");
+    Serial.println("end loop action");
   }
-
-  delay(1000);
-  Serial.println("end loop");
+  
 }
 
 int getMedianNum(int bArray[], int iFilterLen)
