b23486596d62d1d9c2531027a4d96ed1e766399d
kd993595 <kepedu17@gmail.com>
2024-02-29 20:17:02 -0500

    update main.py

--- DATA-team/main.py
+++ DATA-team/main.py
@@ -14,3 +14,30 @@ r = requests.post(url=API_ENDPOINT, data=data, headers={"Content-Type": "text/cs
 
 print(r.status_code)
 print(r.content)
+
+
+"""
+#combine eventually
+import serial
+import datetime
+import pytz
+
+def nowtime():
+    dtime = datetime.datetime.now()
+    timezone = pytz.timezone("America/New_York")
+    dtzone = timezone.localize(dtime)
+    tstamp = dtzone.timestamp()
+    epoch_time_secs = int(round(tstamp))
+    return epoch_time_secs
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser.reset_input_buffer()
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            tstamp = nowtime()
+            line += ";"+str(tstamp)
+            print(line)
+
+"""
